d42f9aba9dfe47c06810ad1ba77071fec90a8ea8
unknown <nischit819@gmail.com>
2023-08-21 22:48:04 +0530

    zomato

--- src/components/common/delivery/index.js
+++ src/components/common/delivery/index.js
@@ -1,8 +1,44 @@
 import React from "react";
 import "./delivery.css";
+import Filters from "../filters";
+
+const deliveryFilters = [
+  {
+    id: 1,
+    icon: <i className="ri-equalizer-fill"></i>,
+    title: "Filters",
+  },
+  {
+    id: 2,
+    title: "Rating: 4.0+ ",
+  },
+  {
+    id: 3,
+    title: "Safe and Hygienic",
+  },
+  {
+    id: 4,
+    title: "Pure Veg",
+  },
+  {
+    id: 5,
+    title: "Delivery Time",
+    icon: <i className="ri-arrow-up-down-line"></i>,
+  },
+  {
+    id: 6,
+    title: "Great Offers",
+  },
+];
 
 const Delivery = () => {
-  return <div>This is Delivery</div>;
+  return (
+    <div>
+      <div className="max-width">
+        <Filters filterList={deliveryFilters} />
+      </div>
+    </div>
+  );
 };
 
 export default Delivery;
